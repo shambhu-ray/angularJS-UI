ac1a89d5ae2f713baf6cbf55d7b21aec2a1b2e34
Shambhu <sray@isuraksha.in>
2018-05-05 10:41:45 +0530

    baby born

---
+++ app/main.js
@@ -0,0 +1,28 @@
+var app = angular.module("mainApp", ["ngRoute"]);
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "component/home/home.html"
+    })
+    .when("/about", {
+        templateUrl : "component/home/about.html"
+    })
+    .when("/angular", {
+        templateUrl : "component/home/angular.html"
+    })
+    .when("/ionic", {
+        templateUrl : "component/home/ionic.html"
+    })
+    .when("/login", {
+        templateUrl : "component/login/login.html"
+    })
+    .otherwise({
+        redirectTo: '/'
+    });
+});
+app.controller("londonCtrl", function ($scope) {
+    $scope.msg = "I love London";
+});
+app.controller("parisCtrl", function ($scope) {
+    $scope.msg = "I love Paris";
+});
